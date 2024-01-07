ce5445a3ad8560995e52171410cd5d7efab90c57
Leow Wen Bin <41581512+LeowWB@users.noreply.github.com>
2024-01-07 20:05:00 +0800

    Remove feature that automatically gets pinyin via selenium (chromedriver made some changes and i'm lazy to figure out the new api, not like I need that many pinyin anyway)

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-I suppose it's more accurate to call this repo `gtranslate-to-csv` since the output is actually in `.csv` format. Oops.
+Should be called `gtranslate-to-csv` since the output is actually in `.csv` format.
 
-Google Translate has this "Saved Translations" feature, and it also allows you to export your saved translations to Google Sheets. That's pretty useful for someone like me who makes Anki flashcards out of the saved translations. Problem is, the exported Google Sheet doesn't have pinyin. So I made this script to take that exported Google Sheet and to add the pinyin in separately, also using Google Translate but in a Selenium-controlled browser. The output will be a `.csv` file that you can import into Anki.
+Input is a `.csv` file from Google Translate's saved translations. It'll organize it into an output `.csv` file that you can feed to Anki. Pinyins are not included; you have to enter them yourself if you need them. 
 
-To use this, install the dependencies and stuff as usual. Also download the Chrome Webdriver and indicate its path in the script. And remember to convert your saved translations to a Google Sheet, then download that sheet as a `.csv` file. Might wanna change your terminal font too. SimHei works for me.
+Might wanna change your terminal font too. SimHei works for me.
--- main.py
+++ main.py
@@ -1,46 +1,15 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-import os
+# changes to make: if first part of entry is english, then un-capitalize if only the first letter is capitalized. then u can more easily find dupes.
+# if entering pinyin AND english, ask for pinyin first
+
 import time
 import csv
 
-TIMER_MULTIPLIER = 2
-DRIVER_PATH = 'chromedriver/chromedriver.exe'
 CSV_PATH = 'Saved translations - Saved translations.csv'
 OUTPUT_PATH = 'output.csv'
 
 num_lines = 0
 
-# set up driver
-driver = webdriver.Chrome(DRIVER_PATH) 
-page_url = 'https://translate.google.com' 
-driver.get(page_url) 
-driver.maximize_window() 
-driver.implicitly_wait(20*TIMER_MULTIPLIER) 
-time.sleep(1*TIMER_MULTIPLIER)
-
-# choose chinese. i can't use "Detect Language" because some words are interpreted as japanese.
-choose_lang_button = driver.find_element(by=By.XPATH, value='/html/body/c-wiz/div/div[2]/c-wiz/div[2]/c-wiz/div[1]/div[1]/c-wiz/div[1]/c-wiz/div[2]/button')
-choose_lang_button.click()
-time.sleep(1*TIMER_MULTIPLIER)
-chinese_button = driver.find_element(by=By.XPATH, value='//*[@id="yDmH0d"]/c-wiz/div/div[2]/c-wiz/div[2]/c-wiz/div[1]/div[1]/c-wiz/div[2]/c-wiz/div[1]/div/div[3]/div/div[3]/div[20]/div[2]')
-chinese_button.click()
-time.sleep(1*TIMER_MULTIPLIER)
-
-# gets the pinyin of a chinese phrase from google translate
-def get_pinyin(driver, chinese_phrase):
-    textarea = driver.find_element(by=By.XPATH, value='/html/body/c-wiz/div/div[2]/c-wiz/div[2]/c-wiz/div[1]/div[2]/div[3]/c-wiz[1]/span/span/div/textarea')
-    textarea.clear()
-    time.sleep(2*TIMER_MULTIPLIER)
-    textarea.send_keys(chinese_phrase)
-    driver.implicitly_wait(20*TIMER_MULTIPLIER) 
-    time.sleep(2*TIMER_MULTIPLIER) 
-    pinyin_label = driver.find_element(by=By.XPATH, value='/html/body/c-wiz/div/div[2]/c-wiz/div[2]/c-wiz/div[1]/div[2]/div[3]/c-wiz[1]/div[2]/div[1]')
-    pinyin = pinyin_label.get_attribute('innerHTML')
-    return pinyin
-
-def process_line(driver, line):
+def process_line(line):
     if line[0] == 'English':
         english_bit = line[2]
         chinese_bit = line[3]
@@ -50,22 +19,20 @@ def process_line(driver, line):
         chinese_bit = line[2]
         en_to_zh = False
 
-    pinyin = get_pinyin(driver, chinese_bit)
-    
     english_bit = english_bit.replace(',','/').strip()
     chinese_bit = chinese_bit.replace(',','/').strip()
-    pinyin = pinyin.replace(',','/').strip()
 
-    return (english_bit, chinese_bit, pinyin, en_to_zh)
+    return (english_bit, chinese_bit, en_to_zh)
 
 def processed_line_to_output(processed_line):
-    english_bit, chinese_bit, pinyin, en_to_zh = processed_line
+    english_bit, chinese_bit, en_to_zh = processed_line
+    pinyin = ''
     if en_to_zh:
-        print(f'{english_bit} -> {chinese_bit} {pinyin}')
+        print(f'{english_bit} -> {chinese_bit}')
     else:
-        print(f'{chinese_bit} {pinyin} -> {english_bit}')
+        print(f'{chinese_bit} -> {english_bit}')
 
-    user_choice = input("[1] Add\t[2] Add Reversed\t[3] Don't Add\t[4] Change English\t[5] Change Pinyin\n")
+    user_choice = input("[1] Add\t[2] Add Reversed\t[3] Don't Add\t[4] Change English\t[5] Add Pinyin\n")
 
     if '3' in user_choice:
         output = ''
@@ -74,7 +41,7 @@ def processed_line_to_output(processed_line):
             english_bit = input('Enter new English translation: ')
         
         if '5' in user_choice:
-            pinyin = input('Enter new pinyin: ')
+            pinyin = ' ' + input('Enter new pinyin: ')
         
         if en_to_zh:
             first_bit = english_bit
@@ -86,10 +53,10 @@ def processed_line_to_output(processed_line):
         output = ''
 
         if '1' in user_choice:
-            output += f'{first_bit},{second_bit} {pinyin}\n'
+            output += f'{first_bit},{second_bit}{pinyin}\n'
         
         if '2' in user_choice:
-            output += f'{second_bit},{first_bit} {pinyin}\n'
+            output += f'{second_bit},{first_bit}{pinyin}\n'
 
     print(f'Result:\n{output}\n')
     return output
@@ -103,7 +70,7 @@ with open(CSV_PATH, 'r', encoding='utf-8') as csv_file:
     reader = csv.reader(csv_file)
 
     for i, line in enumerate(reader):
-        processed_lines.append(process_line(driver, line))
+        processed_lines.append(process_line(line))
 
         if i % 10 == 0:
             print(f'progress: {i}/{num_lines} ({i/num_lines*100:.2f}%)')
